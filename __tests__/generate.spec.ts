dab804ea33e7b367eeb17fa2b923ed36e5632ccc
Gago <xeroice@gmail.com>
2020-08-03 18:43:43 -0700

    100% tested (#2)

--- __tests__/generate.spec.ts
+++ __tests__/generate.spec.ts
@@ -4,6 +4,7 @@ import * as fg from 'fast-glob';
 import { generateCommand } from '../';
 import { generate } from '../src/commands/generate';
 import { fail, stopAndPersist } from '../__mocks__/ora';
+import { search } from '../__mocks__/cosmiconfig';
 import path from 'path';
 
 jest.mock('fs');
@@ -28,10 +29,12 @@ const withGitIgnore = { ...files, '.gitignore': '../__mocks__/gitIgnore1' };
 type Callback = (err: Error | null, response: unknown) => void;
 describe('Generate', () => {
   beforeEach(() => {
-    sync.mockClear();
+    sync.mockRestore();
     stopAndPersist.mockClear();
     writeFile.mockClear();
     existsSync.mockRestore();
+    search.mockRestore();
+    fail.mockReset();
   });
 
   it('should generate a CODEOWNERS file (re-using codeowners content)', async () => {
@@ -52,11 +55,10 @@ describe('Generate', () => {
     });
 
     await generateCommand({ parent: {}, output: 'CODEOWNERS' });
-    expect(writeFile.mock.calls).toMatchInlineSnapshot(`
+    expect(writeFile.mock.calls[0]).toMatchInlineSnapshot(`
       Array [
-        Array [
-          "CODEOWNERS",
-          "# We are already using CODEOWNERS and we don't want to lose the content of this file.
+        "CODEOWNERS",
+        "# We are already using CODEOWNERS and we don't want to lose the content of this file.
       scripts/ @myOrg/infraTeam
       # We might wanna keep an eye on something else, like yml files and workflows.
       .github/workflows/ @myOrg/infraTeam
@@ -78,7 +80,6 @@ describe('Generate', () => {
       dir2/dir3/*.ts @miny
 
       #################################### Generated content - do not edit! ####################################",
-        ],
       ]
     `);
   });
@@ -118,6 +119,70 @@ describe('Generate', () => {
     `);
   });
 
+  it('should generate a CODEOWNERS FILE with package.maintainers field using cosmiconfig', async () => {
+    search.mockImplementationOnce(() =>
+      Promise.resolve({
+        isEmpty: false,
+        filepath: '/some/package.json',
+        config: {
+          output: 'CODEOWNERS',
+          useMaintainers: true,
+        },
+      })
+    );
+
+    const packageFiles = {
+      ...files,
+      'dir5/package.json': '../__mocks__/package1.json',
+      'dir2/dir1/package.json': '../__mocks__/package2.json',
+      'dir6/package.json': '../__mocks__/package3.json',
+      'dir7/package.json': '../__mocks__/package4.json',
+    };
+
+    sync.mockReturnValueOnce([
+      ...Object.keys(packageFiles),
+      'dir5/package.json',
+      'dir2/dir1/package.json',
+      'dir6/package.json',
+      'dir7/package.json',
+    ]);
+
+    sync.mockReturnValueOnce(['.gitignore']);
+    const withAddedPackageFiles = { ...packageFiles, ...withGitIgnore };
+    readFile.mockImplementation((file: keyof typeof withAddedPackageFiles, callback: Callback) => {
+      const content = readFileSync(path.join(__dirname, withAddedPackageFiles[file]));
+      callback(null, content);
+    });
+
+    await generateCommand({ parent: {} });
+    expect(search).toHaveBeenCalled();
+    expect(writeFile.mock.calls[0][1]).toMatchInlineSnapshot(`
+      "#################################### Generated content - do not edit! ####################################
+      # This block has been generated with codeowners-generator (for more information https://github.com/gagoar/codeowners-generator/README.md)
+      # To re-generate, run npm run codeowners-generator generate. Don't worry, the content outside this block will be kept.
+
+      # Rule extracted from dir5/package.json
+      dir5/ friend@example.com other@example.com
+      # Rule extracted from dir2/dir1/package.json
+      dir2/dir1/ friend@example.com other@example.com
+      # Rule extracted from dir5/package.json
+      dir5/ friend@example.com other@example.com
+      # Rule extracted from dir2/dir1/package.json
+      dir2/dir1/ friend@example.com other@example.com
+      # Rule extracted from dir1/CODEOWNERS
+      dir1/*.ts @eeny @meeny
+      # Rule extracted from dir1/CODEOWNERS
+      dir1/README.md @miny
+      # Rule extracted from dir2/CODEOWNERS
+      dir2/*.ts @moe
+      # Rule extracted from dir2/CODEOWNERS
+      dir2/dir3/*.ts @miny
+      # Rule extracted from dir2/dir3/CODEOWNERS
+      dir2/dir3/*.ts @miny
+
+      #################################### Generated content - do not edit! ####################################"
+    `);
+  });
   it('should generate a CODEOWNERS FILE with package.maintainers field', async () => {
     const packageFiles = {
       ...files,
@@ -143,11 +208,8 @@ describe('Generate', () => {
     });
 
     await generateCommand({ parent: {}, output: 'CODEOWNERS', useMaintainers: true });
-    expect(writeFile.mock.calls).toMatchInlineSnapshot(`
-      Array [
-        Array [
-          "CODEOWNERS",
-          "#################################### Generated content - do not edit! ####################################
+    expect(writeFile.mock.calls[0][1]).toMatchInlineSnapshot(`
+      "#################################### Generated content - do not edit! ####################################
       # This block has been generated with codeowners-generator (for more information https://github.com/gagoar/codeowners-generator/README.md)
       # To re-generate, run npm run codeowners-generator generate. Don't worry, the content outside this block will be kept.
 
@@ -170,9 +232,7 @@ describe('Generate', () => {
       # Rule extracted from dir2/dir3/CODEOWNERS
       dir2/dir3/*.ts @miny
 
-      #################################### Generated content - do not edit! ####################################",
-        ],
-      ]
+      #################################### Generated content - do not edit! ####################################"
     `);
   });
   it('should return rules', async () => {
@@ -256,23 +316,70 @@ describe('Generate', () => {
     `);
   });
 
-  it('should blow up after finding malformed CODEOWNER', async () => {
+  it('should show customConfiguration is malformed', async () => {
+    sync.mockReturnValueOnce(Object.keys(files));
+    sync.mockReturnValueOnce(['.gitignore']);
+
+    const filesWithIgnore = {
+      ...files,
+      ...withGitIgnore,
+    };
+    readFile.mockImplementation((file: keyof typeof filesWithIgnore, callback: Callback) => {
+      const content = readFileSync(path.join(__dirname, filesWithIgnore[file]));
+      callback(null, content);
+    });
+    search.mockImplementationOnce(() => Promise.reject(new Error('some malformed configuration')));
+
+    await generateCommand({ parent: {} });
+    expect(fail.mock.calls).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "We found an error looking for custom configuration, we will use cli options if provided",
+        ],
+      ]
+    `);
+  });
+
+  it('should fail trying to read .gitignore', async () => {
+    sync.mockReturnValueOnce(Object.keys(files));
+    sync.mockReturnValueOnce(['.gitignore']);
+
+    readFile.mockImplementation((file: keyof typeof files, callback: Callback) => {
+      const content = readFileSync(path.join(__dirname, files[file]));
+      callback(null, content);
+    });
+
+    await generateCommand({ parent: {} });
+    expect(writeFile).toHaveBeenCalled();
+  });
+
+  it('should blow up after finding malformed CODEOWNERS', async () => {
     const addedMalformedFiles = {
       ...files,
       'dir4/CODEOWNERS': '../__mocks__/CODEOWNERS4',
     };
-    sync.mockReturnValue([...Object.keys(files), 'dir4/CODEOWNERS']);
+    sync.mockReturnValueOnce([...Object.keys(files), 'dir4/CODEOWNERS']);
+    sync.mockReturnValueOnce([]);
+    search.mockImplementationOnce(() =>
+      Promise.resolve({
+        isEmpty: false,
+        filepath: '/some/package.json',
+        config: {
+          output: 'CODEOWNERS',
+          useMaintainers: true,
+        },
+      })
+    );
 
     readFile.mockImplementation((file: keyof typeof addedMalformedFiles, callback: Callback) => {
       const content = readFileSync(path.join(__dirname, addedMalformedFiles[file]));
       callback(null, content);
     });
+
     await generateCommand({ parent: {}, output: 'CODEOWNERS' });
-    expect(fail.mock.calls).toMatchInlineSnapshot(`
+    expect(fail.mock.calls[0]).toMatchInlineSnapshot(`
       Array [
-        Array [
-          "We encountered an error: Error: *.ts in dir4/CODEOWNERS can not be parsed",
-        ],
+        "We encountered an error: Error: *.ts in dir4/CODEOWNERS can not be parsed",
       ]
     `);
   });
