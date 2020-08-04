f36c995f879d060048b0746255db913f76848750
Gago <xeroice@gmail.com>
2020-08-03 19:17:02 -0700

    making copy return differently depending on yarn.lock

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 import { stripIndents } from 'common-tags';
+import { existsSync } from 'fs';
 export const SUCCESS_SYMBOL = '💫';
 export const SHRUG_SYMBOL = '¯\\_(ツ)_/¯';
 export const OUTPUT = 'CODEOWNERS';
@@ -6,18 +7,26 @@ export const INCLUDES = ['**/CODEOWNERS', '!CODEOWNERS', '!node_modules'];
 export const PACKAGE_JSON_PATTERN = ['**/package.json'];
 export const MAINTAINERS_EMAIL_PATTERN = /<(.+)>/;
 export const IGNORE_FILES_PATTERN = ['.gitignore'];
+
 export const CONTENT_MARK = stripIndents`
 #################################### Generated content - do not edit! ####################################
 `;
-export const CONTENT_LEGEND = stripIndents`
+const CONTENT_LEGEND_NPM = stripIndents`
 # This block has been generated with codeowners-generator (for more information https://github.com/gagoar/codeowners-generator/README.md)
-# To re-generate, run npm run codeowners-generator generate. Don't worry, the content outside this block will be kept. 
+# To re-generate, run \`npm run codeowners-generator generate\`. Don't worry, the content outside this block will be kept. 
 `;
+const CONTENT_LEGEND_YARN = stripIndents`
+# This block has been generated with codeowners-generator (for more information https://github.com/gagoar/codeowners-generator/README.md)
+# To re-generate, run \`yarn codeowners-generator generate\`. Don't worry, the content outside this block will be kept. 
+`;
+
+const getContentLegend = () => (existsSync('./yarn.lock') ? CONTENT_LEGEND_YARN : CONTENT_LEGEND_NPM);
+
 export const contentTemplate = (generatedContent: string, originalContent: string): string => {
   return stripIndents`
   ${originalContent && originalContent}
   ${CONTENT_MARK}
-  ${CONTENT_LEGEND}\n
+  ${getContentLegend()}\n
   ${generatedContent}\n
   ${CONTENT_MARK}\n
 `;
