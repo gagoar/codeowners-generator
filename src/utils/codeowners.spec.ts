95b44dfe47314f8b261c39394ef03450557faf25
Gustav Utterheim <gustav@utterheim.com>
2021-01-21 21:39:50 +0100

    refactor test for less repetition

--- src/utils/codeowners.spec.ts
+++ src/utils/codeowners.spec.ts
@@ -39,123 +39,53 @@ describe('Codeowners', () => {
       await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).rejects.toThrowError(expectedError);
     });
 
-    it('should match * to all file under the given directory and its subdirectories', async () => {
-      readContentMock.mockResolvedValueOnce('* @meeny');
+    const validRuleCases = [
+      ['should match * to all file under the given directory and its subdirectories', '*', 'dir1/**/*'],
+      [
+        'should only match /* to all file in the root of the given directory and not its subdirectories',
+        '/*',
+        'dir1/*',
+      ],
+      ['should match *.ts to all ts file under the given directory and its subdirectories', '*.ts', 'dir1/**/*.ts'],
+      ['should only match /*.ts to ts file in the given directory and not its subdirectories', '/*.ts', 'dir1/*.ts'],
+      [
+        'should match apps/ to any directories named apps in the given directory and its subdirectories',
+        'apps/',
+        'dir1/**/apps/',
+      ],
+      [
+        'should match docs/* to files under directly inside a directory named docs in the given directory',
+        'docs/*',
+        'dir1/docs/*',
+      ],
+      [
+        'should match filenames to files in the given directory and its subdirectories',
+        'README.md',
+        'dir1/**/README.md',
+      ],
+      ['should append globstar patterns to the given directory', '**/something.ts', 'dir1/**/something.ts'],
+      ['should append globstar/glob patterns to the given directory', '**/*.ts', 'dir1/**/*.ts'],
+      ['should append glob patterns with fixed base to the given directory', 'dir2/*.ts', 'dir1/dir2/*.ts'],
+      ['should append static file patterns to the given directory', 'dir2/something.ts', 'dir1/dir2/something.ts'],
+      ['should append static directory patterns (dir2/dir3/) to the given directory', 'dir2/dir3/', 'dir1/dir2/dir3/'],
+      ['should append patterns starting with a slash (/) to the given directory', '/', 'dir1/'],
+      [
+        'should append file patterns starting with a slash (/asd/asd.ts) to the given directory',
+        '/asd/asd.ts',
+        'dir1/asd/asd.ts',
+      ],
+      [
+        'should append glob patterns starting with a slash (/**/asd.ts) to the given directory',
+        '/**/asd.ts',
+        'dir1/**/asd.ts',
+      ],
+    ];
+
+    it.each(validRuleCases)('%s (%s)', async (_name, pattern, expectedGlob) => {
+      readContentMock.mockResolvedValueOnce(`${pattern} @eeny`);
 
       await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/**/*', owners: ['@meeny'] },
-      ]);
-    });
-
-    it('should only match /* to all file in the root of the given directory and not its subdirectories', async () => {
-      readContentMock.mockResolvedValueOnce('/* @miny');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/*', owners: ['@miny'] },
-      ]);
-    });
-
-    it('should match *.ts to all ts file under the given directory and its subdirectories', async () => {
-      readContentMock.mockResolvedValueOnce('*.ts @meeny');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/**/*.ts', owners: ['@meeny'] },
-      ]);
-    });
-
-    it('should only match /*.ts to ts file in the given directory and not its subdirectories', async () => {
-      readContentMock.mockResolvedValueOnce('/*.ts @miny');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/*.ts', owners: ['@miny'] },
-      ]);
-    });
-
-    it('should match apps/ to any directories named apps in the given directory and its subdirectories', async () => {
-      readContentMock.mockResolvedValueOnce('apps/ @moe');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/**/apps/', owners: ['@moe'] },
-      ]);
-    });
-
-    it('should match docs/* to files under directly inside a directory named docs in the given directory', async () => {
-      readContentMock.mockResolvedValueOnce('docs/* @eeny');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/docs/*', owners: ['@eeny'] },
-      ]);
-    });
-
-    it('should match filenames to files in the given directory and its subdirectories', async () => {
-      readContentMock.mockResolvedValueOnce('README.md @meeny');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/**/README.md', owners: ['@meeny'] },
-      ]);
-    });
-
-    it('should append globstar patterns to the given directory', async () => {
-      readContentMock.mockResolvedValueOnce('**/something.ts @miny');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/**/something.ts', owners: ['@miny'] },
-      ]);
-    });
-
-    it('should append globstar/glob patterns to the given directory', async () => {
-      readContentMock.mockResolvedValueOnce('**/*.ts @moe');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/**/*.ts', owners: ['@moe'] },
-      ]);
-    });
-
-    it('should append glob patterns with fixed base to the given directory', async () => {
-      readContentMock.mockResolvedValueOnce('dir2/*.ts @eeny');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/dir2/*.ts', owners: ['@eeny'] },
-      ]);
-    });
-
-    it('should append static file patterns to the given directory', async () => {
-      readContentMock.mockResolvedValueOnce('dir2/something.ts @meeny');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/dir2/something.ts', owners: ['@meeny'] },
-      ]);
-    });
-
-    it('should append static directory patterns (dir2/dir3/) to the given directory', async () => {
-      readContentMock.mockResolvedValueOnce('dir2/dir3/ @miny');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/dir2/dir3/', owners: ['@miny'] },
-      ]);
-    });
-
-    it('should append patterns starting with a slash (/) to the given directory', async () => {
-      readContentMock.mockResolvedValueOnce('/ @moe');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/', owners: ['@moe'] },
-      ]);
-    });
-
-    it('should append file patterns starting with a slash (/asd/asd.ts) to the given directory', async () => {
-      readContentMock.mockResolvedValueOnce('/asd/asd.ts @eeny');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/asd/asd.ts', owners: ['@eeny'] },
-      ]);
-    });
-
-    it('should append glob patterns starting with a slash (/**/asd.ts) to the given directory', async () => {
-      readContentMock.mockResolvedValueOnce('/**/asd.ts @meeny');
-
-      await expect(loadCodeOwnerFiles('/root', ['/root/dir1/CODEOWNERS'])).resolves.toEqual([
-        { filePath: 'dir1/CODEOWNERS', glob: 'dir1/**/asd.ts', owners: ['@meeny'] },
+        { filePath: 'dir1/CODEOWNERS', glob: expectedGlob, owners: ['@eeny'] },
       ]);
     });
   });
